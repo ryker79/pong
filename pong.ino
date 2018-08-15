234e458df26cf48e94adae09e2c6b953b4b3e468
Jeff Rice <jeff.rice@vml.com>
2018-08-15 07:33:13 -0500

    initial commit

---
+++ pong.ino
@@ -0,0 +1,33 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       pong.ino
+    Created:	8/15/2018 7:03:25 AM
+    Author:     Alan Ryker
+*/
+
+// Define User Types below here or use a .h file
+//
+#include <Arduboy.h>
+Arduboy arduboy;
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
