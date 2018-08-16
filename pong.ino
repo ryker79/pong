3bf73caf15c499212416d90f1bd264b96d658998
Jeff Rice <jeff.rice@vml.com>
2018-08-16 08:29:52 -0500

    ball and player paddle up and running

--- pong.ino
+++ pong.ino
@@ -10,16 +10,25 @@
 //
 #include <Arduboy.h>
 Arduboy arduboy;
+
+//variables
 int gamestate = 0;
-bool a_fresh = true;
+bool aFresh = true;
+int border = 1;
 
+int ballX = 3;
+int ballY = 3;
+int ballRadius = 2;
+int ballDiameter = 4;
+int ballSpeed = 1;
+bool ballRight = true;
+bool ballDown = true;
 
-// Define Function Prototypes that use User Types below here or use a .h file
-//
-
-
-// Define Functions below here or use other .ino or cpp files
-//
+int paddleWidth = 4;
+int paddleHeight = 9;
+int playerSpeed = 1;
+int playerX = 2;
+int playerY = 0;
 
 // The setup() function runs once each time the micro-controller starts
 void setup()
@@ -34,6 +43,7 @@ void setup()
 // Add the main program code into the continuous loop() function
 void loop()
 {
+	//Prevent the Arduboy from running too fast
 	if(!arduboy.nextFrame())
 	{
 		return;
@@ -44,43 +54,87 @@ void loop()
 	case 0:
 		arduboy.setCursor(0, 0);
 		arduboy.print("Title Screen");
-		if (arduboy.pressed(A_BUTTON) == true && a_fresh == true)
+		if (arduboy.pressed(A_BUTTON) == true && aFresh == true)
 		{
-			a_fresh = false;
+			aFresh = false;
 			gamestate = 1;
 		}
 		break;
-	case 1:
-		arduboy.setCursor(0, 0);
-		arduboy.print("Gameplay");
-		if (arduboy.pressed(A_BUTTON) == true && a_fresh == true)
+	case 1: //Gameplay
+		arduboy.drawRect(0, 0, WIDTH, HEIGHT, WHITE);
+		arduboy.fillCircle(ballX, ballY, ballRadius, WHITE);
+		if (ballRight == true)
 		{
-			a_fresh = false;
+			ballX = ballX + ballSpeed;
+		} else
+		{
+			ballX = ballX - ballSpeed;
+		}
+
+		if (ballDown == true)
+		{
+			ballY = ballY + ballSpeed;
+		} else
+		{
+			ballY = ballY - ballSpeed;
+		}
+
+		if(ballX == ballRadius + border)
+		{
+			ballRight = true;
+		}
+		if(ballX == WIDTH - ballRadius - border)
+		{
+			ballRight = false;
+		}
+		if(ballY == ballRadius + border)
+		{
+			ballDown = true;
+		}
+		if(ballY == HEIGHT - ballRadius - border)
+		{
+			ballDown = false;
+		}
+
+		arduboy.fillRect(playerX, playerY, paddleWidth, paddleHeight, WHITE);
+		if(arduboy.pressed(UP_BUTTON) && playerY > 0 + border)
+		{
+			playerY = playerY - playerSpeed;
+
+		}
+		if(arduboy.pressed(DOWN_BUTTON) && playerY < (HEIGHT - (paddleHeight + border)))
+		{
+			playerY = playerY + playerSpeed;
+		}
+
+		if (arduboy.pressed(A_BUTTON) == true && aFresh == true)
+		{
+			aFresh = false;
 			gamestate = 2;
 		}
 		break;
 	case 2:
 		arduboy.setCursor(0, 0);
 		arduboy.print("Win Screen");
-		if (arduboy.pressed(A_BUTTON) == true && a_fresh == true)
+		if (arduboy.pressed(A_BUTTON) == true && aFresh == true)
 		{
-			a_fresh = false;
+			aFresh = false;
 			gamestate = 3;
 		}
 		break;
 	case 3:
 		arduboy.setCursor(0, 0);
 		arduboy.print("Game Over Screen");
-		if (arduboy.pressed(A_BUTTON) == true && a_fresh == true)
+		if (arduboy.pressed(A_BUTTON) == true && aFresh == true)
 		{
-			a_fresh = false;
-			gamestate = 1;
+			aFresh = false;
+			gamestate = 0;
 		}
 		break;
 	}
 	if (arduboy.notPressed(A_BUTTON) == true)
 	{
-		a_fresh = true;
+		aFresh = true;
 	}
 
 	arduboy.display();
